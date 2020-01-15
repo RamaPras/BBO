d38bdf381e25ffda8586fb66d72477fd9c772329
RamaPras <rama96pras@gmail.com>
2020-01-15 08:47:11 +0700

    2020-01-13

--- Aplikasi_DPK_2020_01_13/application/views/script/Cif_Script.php
+++ Aplikasi_DPK_2020_01_13/application/views/script/Cif_Script.php
@@ -118,7 +118,7 @@
                     },
         "processing": true, //Feature control the processing indicator.
         "serverSide": true, //Feature control DataTables' server-side processing mode.
-        "order": [], //Initial no order.
+        "ordering": false, //Initial no order.
         "sDom": "lfrti",  //menghilangkan paging
         "searching" :false,
         // Load data for the table's content from an Ajax source
